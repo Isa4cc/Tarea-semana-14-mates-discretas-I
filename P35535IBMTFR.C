3bf19c0821a39c1ceadbcd85be52579c9a9652cd
Isaac Benítez <francisco.malquin@outlook.es>
2025-01-10 22:18:51 -0500

    Añado gitignore y arreglo de void, ya no tiene el return 0;

--- P35535IBMTFR.C
+++ P35535IBMTFR.C
@@ -7,3 +7,17 @@ el usuario ingresa una cadena y la invertimos
 
 #include <stdio.h>
 #include "C:\Users\franc\C_FOLDER\SEM-14-MATES-DISC-I\Funciones.h.txt"
+
+int main(){
+
+    int opcion,numero;
+    char cadena[100];
+    
+    printf("\n***************************\n");
+    printf("\nDEBER MATEMATICAS DISCRETAS\n");
+    printf("\n***************************\n");
+    printf("\nPor favor, seleccione una opcion del menu\n");
+
+
+    return 0;
+}
